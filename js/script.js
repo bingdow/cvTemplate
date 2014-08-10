f720cbb5822614f96710a041249794f5068131c7
unknown <bingdow@gmail.com>
2014-08-10 23:32:52 +0800

    first commit.

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 "use strict";
 $(document).ready(function(){
 	$("#canvas-button").click(function() {
-		$("#navigator").toggleClass('expand');
+		$("#navigator, .container").toggleClass('expand');
 		return false;
 	});
 	$('ul#navigator li a').click(function() {
 		$('body, html').stop();
-		$('body, html').animate({ scrollTop: ($($(this).attr("href")).offset().top) }, "slow");
+		$('body, html').animate({ scrollTop: ($($(this).attr("href")).offset().top -70) }, "slow");
 		return false;
 	});
 });
